34440540b107f48703fc6f7a4b618dc3cdd62275
Angel <santoyox714@gmail.com>
2022-08-10 20:59:38 -0700

    add confetti to content page

--- src/lib/pages/Content.svelte
+++ src/lib/pages/Content.svelte
@@ -3,21 +3,90 @@
   import Text from "$lib/components/Text.svelte";
   import { primaryBackground } from "$lib/utils/constants";
   export let backgroundClass = primaryBackground;
+  import Confetti from "$lib/components/Confetti.svelte";
+ 
 
+  /**------------------------------ below script for tap confetti----------------------------- */
+  const duration = 2000
+  
+  /**
+* @type {any[]}
+*/
+  let things = []
+  /**
+* @type {string | number | NodeJS.Timeout | undefined}
+*/
+  let timeout
 
+  async function moveConfetti(event) {
+    const { target, clientX, clientY } = event
 
+    const elementY = target.getBoundingClientRect().top
+    const elementX = target.getBoundingClientRect().left
 
+    const x = clientX - elementX
+    const y = clientY - elementY
+
+    things = [...things, { x, y }]
+
+    clearTimeout(timeout)
+
+    timeout = setTimeout(() => things = [], duration)
+  }
   
 </script>
 
-<Page id="content" title=" " {backgroundClass}>
-  <Text>
-    Content!
-  </Text>
+
+<!--confetti click should encapsulate page-->
+<div class="box" on:click={moveConfetti}>
+  {#each things as thing}
+    <div class="mover" style="left: {thing.x}px; top: {thing.y}px">
+      <Confetti y={[-0.5, 0.5]} fallDistance=20px amount=10 {duration} />
+    </div>
+  {/each}
+<Page id="content" title=" " {backgroundClass}><!-- edit page below-->
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 </Page>
+</div>
 
 
 <style>
+  .box {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      position: relative;
+      height: 100%;
+      width: 100%;
+      
+      background: none;
+      overflow: hidden;
+    }
   
+    .mover {
+      position: absolute;
+    }
+  
+    span {
+      pointer-events: none;
+    }
 </style>
 
