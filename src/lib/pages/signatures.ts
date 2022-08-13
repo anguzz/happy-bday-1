93b6ad5e5ef5c358cc609ecca353292d4bfbb2ea
Angel <santoyox714@gmail.com>
2022-08-13 00:09:50 -0700

    sorted names by alphabet

--- src/lib/pages/signatures.ts
+++ src/lib/pages/signatures.ts
@@ -1,25 +1,13 @@
  export const signatures = [
     {
-        name: 'Ray',
-        message: 'Happy Bday Honey! You are the light of my life, the love of my world, and the sussy of my baka. I love you to all the stars and back!',
-        message2: 'Love - Ray',
+        name: 'Aaron',
+        message: "HAPPY BIRTHDAY STEPH, you are my favorite drug dealer. ",
+        message2: 'Thanks for always being a homie to talk to. ',
         link: ''
     },
     {
-        name: 'Ozzy ',
-        message: 'Hap birth steph hope you have a great day today and may God give you many more years of life',
-        message2: '',
-        link: ''
-    },
-    {
-        name: 'Kinsey',
-        message: 'HAPPY BDAY STEPH THE GREATEST STREAMER/DOMMY MOMMY/JAR JAR BINKS EVER KNOWN TO EXISTENCE',
-        message2: '',
-        link: ''
-    },
-    {
-        name: 'Jacob C.',
-        message: 'Steph you better not just have a happy 👏 rockin 👏 super 👏 happy 👏 groovy 👏 happy 👏 birthDAY...........but a whole year!!!',
+        name: 'Angela',
+        message: "Hi Stepth. Happy Birthday!!!! Hope you have lots of fun today and that the upcoming days are filled with happiness. It's your special day whoooo 🥳🎊!! ",
         message2: '',
         link: ''
     },
@@ -36,44 +24,8 @@
         link: 'https://youtu.be/g7vd_Oy8ixw'
     },
     {
-        name: 'Najat',
-        message: 'happy fuckin birthdayyyy. thank you for encouraging me to fix my incapacitated brain. if i hadn’t tried your medication i wouldn’t have known how loud and obnoxious my fuckin head is.',
-        message2: 'you’re a wonderful person (with wonderful tiddies, can i say that?) and that’s only one of the many things i admire about you. happy birthday Steph :D ',
-        link: ''
-        
-    },
-    {
-        name: 'Definitely Not Iggy',
-        message: "Happy Birthday Steph! I hope you have many more days o' birf and enjoy 'em all.",
-        message2: 'P.S. Fuck bitches, get money!',
-        link: ''
-    },
-    {
         name: 'Chris',
         message: "Happy dommy mommy day!",
-        message2: '',
-        link: ''
-    },
-    {
-        name: 'Shaleen',
-        message: "Happy Birthday Steph! You are a really respectful, kind, funny, and loving friend and I could not be more thankful to call you my friend. I wish you a lot of happiness and success in your life and you are destined to do incredible things! Love you!",
-        message2: 'Regards, Shaleen Mathur',
-        link: ''
-    },
-    {
-        name: 'Mike',
-        message: "AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH",
-        message2: 'happ birf, i wish you so much good dick over the next 10 years. only 10. ',
-        link: 'https://mikeploythai.com/youtube'
-    }, {
-        name: 'Aaron',
-        message: "HAPPY BIRTHDAY STEPH, you are my favorite drug dealer. ",
-        message2: 'Thanks for always being a homie to talk to. ',
-        link: ''
-    },
-    {
-        name: 'Angela',
-        message: "Hi Stepth. Happy Birthday!!!! Hope you have lots of fun today and that the upcoming days are filled with happiness. It's your special day whoooo 🥳🎊!! ",
         message2: '',
         link: ''
     },
@@ -87,6 +39,55 @@
         name: 'Gerry',
         message: "Happy birthday steph! Ive known you for about 2 years and so far you have left a huge impact on my life. U really understand My sense of humor and its always fun having a conversation with you. They get p weird but thats the best part hahahaha. I wish you the best and hope to make more memories as the years continue to come.  ",
         message2: '-Gerry (racist)',
+        link: ''
+    },
+    {
+        name: 'Iggy',
+        message: "Happy Birthday Steph! I hope you have many more days o' birf and enjoy 'em all. P.S. Fuck bitches, get money!",
+        message2: ' -Definitely Not Iggy ',
+        link: ''
+    },
+    {
+        name: 'Jacob C.',
+        message: 'Steph you better not just have a happy 👏 rockin 👏 super 👏 happy 👏 groovy 👏 happy 👏 birthDAY...........but a whole year!!!',
+        message2: '',
+        link: ''
+    },
+    {
+        name: 'Kinsey',
+        message: 'HAPPY BDAY STEPH THE GREATEST STREAMER/DOMMY MOMMY/JAR JAR BINKS EVER KNOWN TO EXISTENCE',
+        message2: '',
+        link: ''
+    },
+    {
+        name: 'Mike',
+        message: "AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH",
+        message2: 'happ birf, i wish you so much good dick over the next 10 years. only 10. ',
+        link: 'https://mikeploythai.com/youtube'
+    },
+    {
+        name: 'Najat',
+        message: 'happy fuckin birthdayyyy. thank you for encouraging me to fix my incapacitated brain. if i hadn’t tried your medication i wouldn’t have known how loud and obnoxious my fuckin head is.',
+        message2: 'you’re a wonderful person (with wonderful tiddies, can i say that?) and that’s only one of the many things i admire about you. happy birthday Steph :D ',
+        link: ''
+        
+    },
+    {
+        name: 'Ozzy ',
+        message: 'Hap birth steph hope you have a great day today and may God give you many more years of life',
+        message2: '',
+        link: ''
+    },
+    {
+        name: 'Ray',
+        message: 'Happy Bday Honey! You are the light of my life, the love of my world, and the sussy of my baka. I love you to all the stars and back!',
+        message2: 'Love - Ray',
+        link: ''
+    },
+    {
+        name: 'Shaleen',
+        message: "Happy Birthday Steph! You are a really respectful, kind, funny, and loving friend and I could not be more thankful to call you my friend. I wish you a lot of happiness and success in your life and you are destined to do incredible things! Love you!",
+        message2: 'Regards, Shaleen Mathur',
         link: ''
     },
 ]
